766ca9f94ef962f5b8442212943e22ba74fbbe0c
JavaScriptUser <3300739800@qq.com>
2020-11-16 16:38:58 +0800

    完善用户管理页面

--- src/views/child/HomeUsers.vue
+++ src/views/child/HomeUsers.vue
@@ -254,7 +254,7 @@ export default {
     },
     async deleteUser(id) {
       let res = await this.$confirm(
-        '此操作将永久删除该文件, 是否继续?',
+        '此操作将永久删除该用户, 是否继续?',
         '提示',
         {
           confirmButtonText: '确定',
